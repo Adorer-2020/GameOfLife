8c8fb10305b7b92766413118710328b6cddcb24d
顾诺衿 <2560636495@qq.com>
2021-05-13 22:33:11 +0800

    optimize

---
+++ src/com/game/Unit/GameTest.java
@@ -0,0 +1,26 @@
+package com.game.Unit;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void getNeighborCount() {
+
+    }
+
+    @Test
+    void gameCycle() {
+
+    }
+
+    @Test
+    void getStatue() {
+
+    }
+
+    @Test
+    void updateMap() {
+
+    }
+}
